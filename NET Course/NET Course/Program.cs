14cda76626cdb854fd4617aeae655569e6fdea08
BluffBatton <havchik17@gmail.com>
2025-03-05 20:23:17 +0200

    Custom list was created

--- NET Course/NET Course/Program.cs
+++ NET Course/NET Course/Program.cs
@@ -1,10 +1,43 @@
-﻿namespace NET_Course
+﻿using System.Collections.Generic;
+
+namespace NET_Course
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Beep-boop-boop");
+            CustomList<int> customList = new CustomList<int>();
+            var list = customList;
+            list.Add(10);
+            list.Add(20);
+            list.Add(30);
+            list.Add(40);
+            list.Add(50);
+            Console.WriteLine($"Count: {list.Count}, Capacity: {list.Capacity}");
+
+            ListForEachTest(customList);
+
+            list.Remove(30);
+            Console.WriteLine("Removed number 30");
+            ListForEachTest(customList);
+            Console.WriteLine("Getting number by index 1: " + list[5]);
+            Console.WriteLine("Попытка выхода за границы:");
+            try
+            {
+                Console.WriteLine(list[10]); // Ожидаем исключение
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ошибка: " + ex.Message);
+            }
+        }
+
+        internal static void ListForEachTest(CustomList<int> customList)
+        {
+            foreach (var item in customList)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
