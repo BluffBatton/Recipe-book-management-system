def38203ee7354e1adec33844ff852103d38326d
BluffBatton <havchik17@gmail.com>
2025-03-22 11:03:51 +0200

    Final changes in CA + delegates

--- NET Course/Core/Models/Recipe.cs
+++ NET Course/Core/Models/Recipe.cs
@@ -2,7 +2,7 @@
 {
     public class Recipe
     {
-        public int RecipeID { get; set; }
+        public int Id { get; set; }
 
         public string? Title { get; set; }
 
--- NET Course/NET Course/Program.cs
+++ NET Course/NET Course/Program.cs
@@ -1,35 +1,10 @@
-﻿using Infrastructure.Collections;
-
-namespace NET_Course
+﻿namespace NET_Course
 {
     internal class Program
     {
         static void Main()
         {
-            CustomList<int> customList = new CustomList<int>();
-            var list = customList;
-            list.Add(10);
-            list.Add(20);
-            list.Add(30);
-            list.Add(40);
-            list.Add(50);
-            Console.WriteLine($"Count: {list.Count}, Capacity: {list.Capacity}");
-
-            ListForEachTest(customList);
-
-            //int dodik = ;
-
-            list.Remove(30);
-            Console.WriteLine("Removed number 30");
-            ListForEachTest(customList);
-            Console.WriteLine("Getting number by index 1: " + list[1]);
-        }
-        internal static void ListForEachTest(CustomList<int> customList)
-        {
-            foreach (var item in customList)
-            {
-                Console.WriteLine(item);
-            }
+            Console.WriteLine("буп бип буп");
         }
     }
 }
