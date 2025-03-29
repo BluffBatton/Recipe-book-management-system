e2f788924ea2a4128f5d15510891003aff744f63
BluffBatton <havchik17@gmail.com>
2025-03-29 19:56:13 +0200

    Ji Son was implemented(along with user repository)

---
+++ NET Course/Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,44 @@
+﻿using Core.Models;
+using System.Text.Json;
+
+namespace Infrastructure.Repositories
+{
+    public static class UserRepository
+    {
+        private readonly static string filePath = "C:\\Users\\havch\\Desktop\\.NET-Course\\.NET-Course\\users.json";
+
+        public static List<User> ReadUsers()
+        {
+            if (!File.Exists(filePath))
+            {
+                return new List<User>();
+            }
+            string json = File.ReadAllText(filePath);
+            return JsonSerializer.Deserialize<List<User>>(json) ?? new List<User>();
+        }
+
+        public static void SaveUsers(List<User> users)
+        {
+            string json = JsonSerializer.Serialize(users, new JsonSerializerOptions { WriteIndented = true });
+            File.WriteAllText(filePath, json);
+        }
+
+        public static void CreateUser(User user)
+        {
+            var users = ReadUsers();
+            users.Add(user);
+            SaveUsers(users);
+        }
+
+        public static void DeleteUser(int userID)
+        {
+            var users = ReadUsers();
+            var userToDelete = users.FirstOrDefault(u => u.Id == userID);
+            if (userToDelete != null)
+            {
+                users.Remove(userToDelete);
+                SaveUsers(users);
+            }
+        }
+    }
+}
--- NET Course/NET Course/Program.cs
+++ NET Course/NET Course/Program.cs
@@ -1,10 +1,41 @@
-﻿namespace NET_Course
+﻿using Core.Models;
+using Infrastructure.Repositories;
+
+namespace NET_Course
 {
     internal class Program
     {
         static void Main()
         {
-            Console.WriteLine("буп бип буп");
+            //List<User> newUsers = new List<User>
+            //{
+            //    new User { Id = 1, Name = "Sanek" },
+            //    new User { Id = 2, Name = "Mykyta" },
+            //    new User { Id = 3, Name = "Danylo" },
+            //};
+
+            //foreach(var user in newUsers)
+            //{
+            //    UserRepository.CreateUser(user);
+            //}
+            //Console.WriteLine("User has been added");
+
+            //var users = UserRepository.ReadUsers();
+            //Console.WriteLine("List of users:");
+            //foreach (var user in users)
+            //{
+            //    Console.WriteLine($"{user.Id}: {user.Name}");
+            //}
+
+            //UserRepository.DeleteUser(1);
+            //Console.WriteLine("User has been deleted");
+
+            var users = UserRepository.ReadUsers();
+            Console.WriteLine("List of users:");
+            foreach (var user in users)
+            {
+                Console.WriteLine($"{user.Id}: {user.Name}");
+            }
         }
     }
 }
