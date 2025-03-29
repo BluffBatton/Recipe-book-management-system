73d0aea912abe5e723db13fe80787254470bcbf7
BluffBatton <havchik17@gmail.com>
2025-03-29 18:51:28 +0200

    Bla bla bla

--- NET Course/NET Course/CustomList.cs
+++ NET Course/NET Course/CustomList.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections;
 using System.Runtime.CompilerServices;
-
-
 namespace NET_Course
 {
     public class CustomList<T> : IEnumerable<T>
